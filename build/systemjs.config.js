95be0f059a58df8b1e88abd02516466de623097f
sdkraemer <tfcmaster9@gmail.com>
2016-08-18 16:40:02 -0700

    Working with gulp

--- build/systemjs.config.js
+++ build/systemjs.config.js
@@ -8,9 +8,9 @@
   var map = {
     'app':                        'app', // 'dist',
 
-    '@angular':                   'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    '@angular':                   'lib/@angular',
+    'angular2-in-memory-web-api': 'lib/angular2-in-memory-web-api',
+    'rxjs':                       'lib/rxjs'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
